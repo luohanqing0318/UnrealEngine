2e5251f7fbdd6b3125ce3890f1c8dbf6f36d226b
TommyLaw <1104430890@qq.com>
2022-06-21 01:24:30 +0800

    建立工程

---
+++ DemoProgram/Source/DemoProgram/DemoProgram.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DemoProgram.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DemoProgram, "DemoProgram" );
---
+++ DemoProgram/Source/DemoProgram/DemoProgramGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DemoProgramGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DEMOPROGRAM_API ADemoProgramGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
